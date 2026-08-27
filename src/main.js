d6e378e07e3b85a046f54cd57d3b068065e63e05
Lenka Brozmanova <brozmanova.lenka@gmail.com>
2026-08-27 12:36:06 +0200

    Refresh hero content module

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { portfolio } from "./data/portfolio.js";
+import { portfolio } from "./data/portfolio.js?v=hero-2";
 import { renderAtlas, renderCaseStudy, renderPartnerGroups, renderPrinciples } from "./components/render.js";
 
 const { hero, profile, atlas, caseStudies, partnerGroups, principles } = portfolio;
